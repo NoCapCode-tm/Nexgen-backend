61ac2af4511d0ab005125eba60c6c90f45591564
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-03-24 16:40:42 +0530

    changes in authmiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,26 @@
 import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
 
-export const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
-
+export const protect = async (req, res, next) => {
   try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+
     next();
-  } catch (error) {
+  } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
