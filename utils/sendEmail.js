194cd4c0c8842f0fac58448e361e48f91fa5574e
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-04-09 14:40:54 +0530

    nodemailler added with config

---
+++ utils/sendEmail.js
@@ -0,0 +1,20 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.nocapcode.cloud",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendEmail = async ({ to, subject, html }) => {
+  await transporter.sendMail({
+    from: `"NoCapCode" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+};
